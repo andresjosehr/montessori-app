88ffc7f5a137f9a8767d9702cc7db22fe7733a5e
José Andrés <joseandreshernandezross@gmail.com>
2023-01-25 12:59:26 -0400

    Cambios realizados

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
 		app: 'directory',
-		api: 'https://projects.andresjosehr.com/montessori-api/api',
+		api: 'https://projects.andresjosehr.com/montessori-api/api/index.php',
 		assets: 'https://projects.andresjosehr.com/montessori-api',
 };
